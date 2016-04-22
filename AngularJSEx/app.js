21393edab750b416d81068b72ae68c9b0673f992
vinay duvvada <vinay.chowdary518@gmail.com>
2016-04-21 18:25:40 -0700

    added UI directives

---
+++ AngularJSEx/app.js
@@ -0,0 +1,7 @@
+(function(){
+	var app = angular.module("githubViewer", []);
+	
+	app.config( function() {
+		
+	});
+}());
--- AngularJSEx/script.js
+++ AngularJSEx/script.js
@@ -5,9 +5,9 @@
         .module("githubViewer", [])
         .controller("MainController", MainController);
 
-    MainController.$inject = ["$scope","$http"];
+    MainController.$inject = ["$scope","$http","$interval","$log","$anchorScroll","$location"];
 
-    function MainController($scope,$http) {
+    function MainController($scope,$http,$interval,$log,$anchorScroll,$location) {
     	var onUserComplete = function(response) {
     		$scope.user = response.data;
             $http.get($scope.user.repos_url)
@@ -16,21 +16,42 @@
 
         var onRepos = function(response) {
             $scope.repos = response.data;
+            $location.hash("userDetails");
+            $anchorScroll();
         };
 
     	var onError = function(reason) {
     		$scope.error = 'Could not fetch the user';
-    	}
+    	};
+
+        var decrementCountdown = function() {
+            $scope.countdown -=1;
+            if($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        }
+
+        var countdownInterval = null;
+        var startCountdown = function() {
+            countdownInterval = $interval(decrementCountdown,1000,$scope.countdown)
+        }
 
         $scope.search = function(username) {
+            $log.info("searching for "+username);
             $http.get("https://api.github.com/users/"+username)
                 .then(onUserComplete,onError);
-            console.log("https://api.github.com/users/"+$scope.username);
+            if(countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+            //console.log("https://api.github.com/users/"+$scope.username);
         }
 
         $scope.username = "angular";
         $scope.message = "Github Viewer";
         $scope.repoSortOrder = "-name";
+        $scope.countdown = 5;
+        startCountdown();
     }
 
 })();
